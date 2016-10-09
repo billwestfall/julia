0626e09c9de0fd1a5e804256344ad490a32f7c32
billw@concur.com <billw@concur.com>
2016-10-09 14:02:48 -0700

    add wikipedia example

--- hello/hello.jl
+++ hello/hello.jl
@@ -1,3 +1,5 @@
 # run as julia hello.jl
 println("hello world")
-
+# from https://en.wikipedia.org/wiki/Julia_%28programming_language%29
+p(x) = 2x^2 + 1; f(x, y) = 1 + 2p(x)y
+println("Hello world!", " I'm on cloud ", f(0, 4), " as Julia supports recognizable syntax!")
