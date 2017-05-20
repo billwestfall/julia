1e6a9f9b14be42f654f80f554256c35292c169c4
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 15:23:12 -0700

    Update doors.jl

--- miscellaneous/rosetta/doors.jl
+++ miscellaneous/rosetta/doors.jl
@@ -1,9 +1,11 @@
 #run as /Applications/Julia-0.5.app/Contents/Resources/julia/bin/julia doors.jl
 
-i = 1; doors = 99
+i = 1; doors = 100
 
-while i <99
+while i <100
+           if i = doors
            println(doors)
+           else
            i = i + 1
            doors = doors - 1
 end
