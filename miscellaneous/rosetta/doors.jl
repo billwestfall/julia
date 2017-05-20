b9d1ae4592321149030a0c16b0466be70dfb53b3
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 14:46:48 -0700

    Create doors.jl

---
+++ miscellaneous/rosetta/doors.jl
@@ -0,0 +1,9 @@
+#run as /Applications/Julia-0.5.app/Contents/Resources/julia/bin/julia doors.jl
+
+i = 1; doors = 99
+
+while i <99
+           println(doors)
+           i = i + 1
+           doors = doors - 1
+end
