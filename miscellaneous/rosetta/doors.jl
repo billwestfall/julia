070c17354dcb011b1610fe4c6c8a9b6233a9e1dd
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 15:51:53 -0700

    Update doors.jl

--- miscellaneous/rosetta/doors.jl
+++ miscellaneous/rosetta/doors.jl
@@ -1,14 +1,12 @@
 #run as /Applications/Julia-0.5.app/Contents/Resources/julia/bin/julia doors.jl
 
 i = 1; doors = rand(1:100)
-#doors = rand(1:100)
 
 while i <100
-#for i in 0:1:100
-if i == doors
-println(doors)
+        if i == doors
+                println(doors)
         break
 else
-    i = i + 1
-end
+        i = i + 1
+        end
 end
